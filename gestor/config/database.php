678f88bdefb0a61dc7d6ce082bbeb148fbd3a43c
gabtryy <gabriel72gx@gmail.com>
2026-06-26 03:11:46 -0400

    Delete gestor directory

--- gestor/config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'host' => '127.0.0.1',
-    'db'   => 'sistema_papeleria',
-    'user' => 'root',
-    'pass' => '',
-    'charset' => 'utf8mb4',
-];
--- gestor/vista/layouts/main.php
+++
@@ -1,6 +0,0 @@
-<?php require __DIR__ . '/header.php'; ?>
-<?php require __DIR__ . '/sidebar.php'; ?>
-<?php require $contentView; ?>
-        </main>
-    </div>
-<?php require __DIR__ . '/footer.php'; ?>
