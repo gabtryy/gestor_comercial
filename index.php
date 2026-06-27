6df2e96e32a9f8dd86eb0d57579655b449799fa8
gabriel <gabriel72gx@gmail.com>
2026-06-26 22:06:51 -0400

    arreglando index

--- index.php
+++ index.php
@@ -25,29 +25,4 @@ require_once BASE_PATH . '/core/Router.php';
 
 Router::dispatch();
 
-<?php
 
-session_start();
-
-define('BASE_PATH', __DIR__);
-
-spl_autoload_register(function (string $class): void {
-    $paths = [
-        BASE_PATH . '/core/' . $class . '.php',
-        BASE_PATH . '/controlador/' . $class . '.php',
-        BASE_PATH . '/modelo/' . $class . '.php',
-    ];
-
-    foreach ($paths as $path) {
-        if (file_exists($path)) {
-            require_once $path;
-            return;
-        }
-    }
-});
-
-require_once BASE_PATH . '/core/Model.php';
-require_once BASE_PATH . '/core/Controller.php';
-require_once BASE_PATH . '/core/Router.php';
-
-Router::dispatch();
